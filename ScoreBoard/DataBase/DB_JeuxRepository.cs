05d56eaf8963ec7151d5cde6138e41a839bf7abe
phili <0758510@cegepoutaouais.qc.ca>
2025-06-12 16:30:06 -0400

    Début méthode ajouter dans DB_JeuxRep, Manque des trucs dans le seeders!!!

---
+++ ScoreBoard/DataBase/DB_JeuxRepository.cs
@@ -0,0 +1,50 @@
+﻿using ScoreBoard.Models;
+
+namespace ScoreBoard.DataBase
+{
+    public class DB_JeuxRepository : IJeu
+    {
+        private readonly DB_ScoreBoardContext _context;
+        public DB_JeuxRepository(DB_ScoreBoardContext context)
+        {
+            _context = context; // Récupération du contexte de la base de données pour le service
+        }
+        public List<Jeu> ListeJeux => _context.Jeux.ToList();
+
+        public void Ajouter(Jeu jeu)
+        {
+            Joueur joueurActif = _context.Joueurs.FirstOrDefault(j => j.Id == jeu.JoueurId); // Récupération du joueur actif
+            if (joueurActif != null)
+            {
+                joueurActif.Jeux.Add(jeu); // Ajout du jeu à la liste des jeux du joueur actif
+                _context.Jeux.Add(jeu); // Ajout du jeu à la base de données
+                _context.SaveChanges(); // Enregistrement des modifications dans la base de données
+            }
+            else
+            {
+                throw new InvalidOperationException("Joueur actif non trouvé."); // Gestion d'erreur si le joueur actif n'est pas trouvé
+
+            }
+        }
+
+        public Jeu? GetJeu(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Jeu> GetJeuxParJoueur(int joueurId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modifier(Jeu jeu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Supprimer(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ScoreBoard/Models/IJeu.cs
@@ -0,0 +1,12 @@
+﻿namespace ScoreBoard.Models
+{
+    public interface IJeu
+    {
+        List<Jeu> ListeJeux { get; }
+        public Jeu? GetJeu(int id);
+        public void Ajouter(Jeu jeu);
+        public void Modifier(Jeu jeu);
+        public void Supprimer(int id);
+        public List<Jeu> GetJeuxParJoueur(int joueurId); // Pour obtenir les jeux d'un joueur spécifique
+    }
+}
